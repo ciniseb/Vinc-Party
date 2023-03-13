1218714e75e9936daf36c92a46b3b688c407540f
ciniseb <sebastienst-denis@hotmail.com>
2023-03-12 22:27:42 -0400

    ...

--- PC_VS/JeuP11/JeuP11.cpp
+++ PC_VS/JeuP11/JeuP11.cpp
@@ -54,63 +54,7 @@ int main()
     
     //tests.tests_fenetrePointages();
     // tests.tests_autre();
-    /*es.demarrer();
-    while (true) {
-        while (es.evenementDisponible()) {
-            std::unique_ptr<Evenement> e = es.prochainEvenement();
-            if (e->getCode() == BOUTON) {
-                Dieu nom = static_cast<Bouton*>(e.get())->getNom();
-                es.envoyerEvenement(std::make_unique<Bargraph>(nom));
-                switch (nom)
-                {
-                case Dieu::D: cout << "D" << endl;
-                    
-                    break;
-                case Dieu::I: cout << "I" << endl;
-                    break;
-                case Dieu::E: cout << "E" << endl;
-                    break;
-                case Dieu::U: cout << "U" << endl;
-                    break;
-                default:
-                    break;
-                }
-                
-            }
-            if (e->getCode() == JOYSTICK) {
-                Direction direction = static_cast<Joystick*>(e.get())->getDirection();
-                switch (direction)
-                {
-                case HAUT: cout << "HAUT" << endl;
-
-                    break;
-                case BAS: cout << "BAS" << endl;
-                    break;
-                case GAUCHE: cout << "GAUCHE" << endl;
-                    break;
-                case DROITE: cout << "DROITE" << endl;
-                    break;
-                default:
-                    break;
-                }
-            }
-
-        }
-
-        es.envoyerEvenement(std::make_unique<Boussole>(NORD));
-        Sleep(1000);
-        es.envoyerEvenement(std::make_unique<Boussole>(SUD));
-        Sleep(1000);
-        es.envoyerEvenement(std::make_unique<Boussole>(EST));
-        Sleep(1000);
-        es.envoyerEvenement(std::make_unique<Boussole>(OUEST));
-        Sleep(1000);
-
-
-        es.envoyerEvenement(std::make_unique<Vibration>());
-        Sleep(1000);
-        
-    }*/
+    
     return 0;
 }
 
